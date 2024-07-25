b5bf4138016ad623c02a639c3e866e03bdf92e38
Javad Yousefi <javadyousefi9000@gmail.com>
2024-07-25 21:26:14 +0330

    add file type

--- src/modules/tools/tools.routes.js
+++ src/modules/tools/tools.routes.js
@@ -7,10 +7,25 @@ const router = require("express").Router();
 const { ToolsController } = require('./tools.controller')
 // Set up Multer for file uploads
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+
+const maxSize = 5048576;
+
+
+const upload = multer({
+  storage: storage, limits: {
+      fileSize: maxSize
+  }, fileFilter: function (req, file, cb) {
+      if (file.mimetype.startsWith("image/")) {
+          cb(null, true);
+      } else {
+          cb(new Error('Invalid file type just accept image types !'), false);
+      }
+  }
+});
 
 router.post('/decrease-image-resolution', upload.single('image'), ToolsController.decreaseImageResolution);
 
 module.exports = {
   toolsRoutes: router
 }
+
